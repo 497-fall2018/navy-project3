17489d1a295ef65626e3c5f9824e28de08eed8ae
Matt Moderwell <mmoderwell@gmail.com>
2018-12-06 22:26:06 -0600

    Styling changes to home screen

--- app/src/components/ItemDisplay/ItemDisplay.js
+++ app/src/components/ItemDisplay/ItemDisplay.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Comments from 'react-native-comments';
 import {
-    Container, Header, Content, Button, Text, Card, CardItem, Body
+	Container,
+	Header,
+	Content,
+	Button,
+	Text,
+	Card,
+	CardItem,
+	Body,
+	Icon
 } from 'native-base';
 import { NavigationActions } from 'react-navigation';
 import { View, ScrollView, TouchableOpacity, Image, RefreshControl, Alert } from 'react-native';
@@ -9,9 +17,9 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 import GestureRecognizer from 'react-native-swipe-gestures';
 import {
-    submit_new_vote_buy,
-    submit_new_vote_nah,
-    toggle_full_text
+	submit_new_vote_buy,
+	submit_new_vote_nah,
+	toggle_full_text
 } from '../../ducks/post';
 
 import SCREEN_IMPORT from 'Dimensions'
@@ -20,36 +28,36 @@ const SCREEN_WIDTH = SCREEN_IMPORT.get('window').width;
 const SCREEN_HEIGHT = SCREEN_IMPORT.get('window').height;
 
 const styles = ({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center'
-    },
-    button: {
-    }
-  });
+	container: {
+		paddingTop: 40,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+	},
+	button: {}
+});
 
 class ItemDisplayComponent extends Component {
-    onSwipeLeft(gestureState) {
-        let post = this.props.posts.filter(x => x['_id'] === this.props.curr_post_id);
-        let new_nah_vote = post[0]['nah']+1;
-        let str = "NAH! \n buy: " + post[0]['buy'].toString() + "\n nah: " + new_nah_vote.toString();
-        Alert.alert(str);
-        this.props.submit_new_vote_nah(new_nah_vote, this.props.curr_post_id);
-    }
-    onSwipeRight(gestureState) {
-        let post = this.props.posts.filter(x => x['_id'] === this.props.curr_post_id);
-        let new_buy_vote = post[0]['buy']+1;
-        let str = "BUY! \n buy: " + new_buy_vote.toString() + "\n nah: " + post[0]['nah'].toString();
-        Alert.alert(str);
-        this.props.submit_new_vote_buy(new_buy_vote, this.props.curr_post_id);
-    }
-    toggleFullText = () => {
-        this.props.toggle_full_text();
-    }
-        render(){
-            return(
-                <Container>
+	onSwipeLeft(gestureState) {
+		let post = this.props.posts.filter(x => x['_id'] === this.props.curr_post_id);
+		let new_nah_vote = post[0]['nah'] + 1;
+		let str = "NAH! \n buy: " + post[0]['buy'].toString() + "\n nah: " + new_nah_vote.toString();
+		Alert.alert(str);
+		this.props.submit_new_vote_nah(new_nah_vote, this.props.curr_post_id);
+	}
+	onSwipeRight(gestureState) {
+		let post = this.props.posts.filter(x => x['_id'] === this.props.curr_post_id);
+		let new_buy_vote = post[0]['buy'] + 1;
+		let str = "BUY! \n buy: " + new_buy_vote.toString() + "\n nah: " + post[0]['nah'].toString();
+		Alert.alert(str);
+		this.props.submit_new_vote_buy(new_buy_vote, this.props.curr_post_id);
+	}
+	toggleFullText = () => {
+		this.props.toggle_full_text();
+	}
+	render() {
+		return (
+			<Container>
                     <Content>
                     <GestureRecognizer
                         onSwipeLeft={() => this.onSwipeLeft()}
@@ -84,31 +92,31 @@ class ItemDisplayComponent extends Component {
                         </Card>
                     </GestureRecognizer>
                     <View  style={styles.container}>
-                        <Button danger large rounded onPress={() => this.onSwipeLeft()}><Text> X </Text></Button>
+                        <Button primary large rounded onPress={() => this.onSwipeRight()} iconLeft><Icon name='thumbs-up' /><Text>Buy</Text></Button>
                         <Button success large rounded><Text> SKIP </Text></Button>
-                        <Button primary large rounded onPress={() => this.onSwipeRight()}><Text> O </Text></Button>
+                        <Button danger large rounded onPress={() => this.onSwipeLeft()} iconLeft><Icon name='thumbs-down' /><Text>Nah</Text></Button>
                     </View>
                     </Content>
                 </Container>
-            );
-        }
-    }
+		);
+	}
+}
 
 export { ItemDisplayComponent };
 
 const mapStateToProps = (state, ownProps) => {
-    const { post } = state;
-    const { curr_post_id, posts, showFullText } = post;
-    return {
-        ...ownProps,
-        curr_post_id,
-        posts,
-        showFullText
-    };
+	const { post } = state;
+	const { curr_post_id, posts, showFullText } = post;
+	return {
+		...ownProps,
+		curr_post_id,
+		posts,
+		showFullText
+	};
 };
 
 export const ItemDisplay = connect(mapStateToProps, {
-    submit_new_vote_buy,
-    submit_new_vote_nah,
-    toggle_full_text
+	submit_new_vote_buy,
+	submit_new_vote_nah,
+	toggle_full_text
 })(ItemDisplayComponent);
